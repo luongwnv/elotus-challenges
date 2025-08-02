cd7c041ac55a8fc03506f6d743b78c9ab5b7909b
Nguyễn Văn Lương <luong.nguyen@viez.vn>
2025-08-02 17:47:58 +0700

    add user model

--- hackathon/authentication-app/internal/models/user.go
+++ hackathon/authentication-app/internal/models/user.go
@@ -2,11 +2,18 @@ package models
 
 import (
 	"time"
+
+	"github.com/google/uuid"
 )
 
 type User struct {
-	ID           uint   `gorm:"primaryKey"`
-	Username     string `gorm:"uniqueIndex;not null"`
-	PasswordHash string `gorm:"not null"`
-	CreatedAt    time.Time
+	ID           uuid.UUID  `gorm:"column:id;primaryKey;type:uuid" json:"id"`
+	Username     string     `gorm:"column:username;uniqueIndex;not null" json:"username"`
+	PasswordHash string     `gorm:"column:password_hash;not null" json:"password_hash"`
+	CreatedAt    time.Time  `gorm:"column:created_at;not null" json:"created_at"`
+	UpdatedAt    *time.Time `gorm:"column:updated_at;not null" json:"updated_at"`
+}
+
+func (User) TableName() string {
+	return "authentication-app.users"
 }
