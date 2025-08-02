60eccc64eedf1ed7e364fc602017f5c7586b622c
Nguyễn Văn Lương <luong.nguyen@viez.vn>
2025-08-02 17:39:06 +0700

    add auth dto

---
+++ hackathon/authentication-app/internal/DTOs/auth_dto.go
@@ -0,0 +1,28 @@
+package dto
+
+import "time"
+
+type RegisterRequest struct {
+	Username string `json:"username" validate:"required,min=3,max=20"`
+	Password string `json:"password" validate:"required,min=6"`
+}
+
+type LoginRequest struct {
+	Username string `json:"username" validate:"required"`
+	Password string `json:"password" validate:"required"`
+}
+
+type AuthResponse struct {
+	Token     string    `json:"token"`
+	ExpiresAt time.Time `json:"expires_at"`
+	User      UserInfo  `json:"user"`
+}
+
+type UserInfo struct {
+	ID       uint   `json:"id"`
+	Username string `json:"username"`
+}
+
+type RevokeTokenRequest struct {
+	TokenID string `json:"token_id" validate:"required"`
+}
